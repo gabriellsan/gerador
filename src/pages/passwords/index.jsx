d919de52e490e83b1ced656cef1046d732677fa7
Gabriel Santana Santos <gabrielsantana123santos@gmail.com>
2023-11-06 02:28:56 -0300

    iniciando a navegação

---
+++ src/pages/passwords/index.jsx
@@ -0,0 +1,15 @@
+import {View, Text, StyleSheet} from 'react-native'
+
+export function Password() {
+    return (
+        <View>
+            <Text>
+                Minhas senhas
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+})
